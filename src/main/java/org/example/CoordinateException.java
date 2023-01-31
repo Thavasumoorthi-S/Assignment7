0fd861f1ef42a54238cad8e51d8ed886063e7ea9
Thavasumoorthi-S <thavasumoorthi.s@tringapps.com>
2023-01-31 10:36:49 +0530

    Seventh commit

--- src/main/java/org/example/CoordinateException.java
+++ src/main/java/org/example/CoordinateException.java
@@ -1,2 +1,45 @@
-package org.example;public class CoordinateException {
+package org.example;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class CoordinateException implements Cloneable {
+    private static final Logger LOGGER = Logger.getLogger("global");
+    int x;
+    int y;
+
+    CoordinateException(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public String equals(CoordinateException c3) {
+        String result;
+        result = this.x == c3.x && this.y == c3.y ? "true" : "false";
+        return result;
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        try {
+            Scanner sc = new Scanner(System.in);
+            LOGGER.log(Level.INFO, "Enter the value x:");
+            int x = sc.nextInt();
+            LOGGER.log(Level.INFO, "Enter the value y:");
+            int y = sc.nextInt();
+            CoordinateException c = new CoordinateException(x, y);
+            CoordinateException c2 = (CoordinateException) c.clone();
+            String s = c.equals(c2);
+            LOGGER.info("Orginal object and clonable object is Same ?: ");
+            LOGGER.info(s);
+        }
+        catch(InputMismatchException e)
+        {
+            throw new InputMismatchException("Please Enter the valid input");
+        }
+    }
 }
--- src/main/java/org/example/CreditcardException.java
+++ src/main/java/org/example/CreditcardException.java
@@ -1,2 +1,71 @@
-package org.example;public class CreditcardException {
+package org.example;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+class CreditcardException implements Cloneable
+{
+    private static final Logger LOGGER=Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    String holdername;
+    Long cardnumber;
+    String expiredate;
+    CreditcardException(String holdername,Long cardnumber,String expiredate)
+    {
+        this.holdername=holdername;
+        this.cardnumber=cardnumber;
+        this.expiredate=expiredate;
+    }
+    public Object clone() throws CloneNotSupportedException
+    {
+        return super.clone();
+    }
+
+    public String equalis(CreditcardException a)
+    {
+        if(cardnumber.equals(a.cardnumber))
+        {
+            return "true";
+        }
+        return "false";
+    }
+    public static void main(String[] args) throws CloneNotSupportedException
+    {
+        try {
+            String holdername;
+            long cardnumber;
+            String expiredate;
+            String holdername1;
+            long cardnumber1;
+            String expiredate1;
+            String result1;
+            String result2;
+            Scanner sc = new Scanner(System.in);
+            LOGGER.log(Level.INFO, "Enter the Name: ");
+            holdername = sc.nextLine();
+            LOGGER.log(Level.INFO, "Enter the cardnumber:  ");
+            cardnumber = sc.nextLong();
+            sc.nextLine();
+            LOGGER.log(Level.INFO, "Enter the expiredate:");
+            expiredate = sc.nextLine();
+            CreditcardException cc = new CreditcardException(holdername, cardnumber, expiredate);
+            CreditcardException cc1 = (CreditcardException) cc.clone();
+            LOGGER.log(Level.INFO, "current object creditcardnumber and clonnable object creditcard numbers are same? :");
+            result1 = cc.equalis(cc1);
+            LOGGER.info(result1);
+            LOGGER.log(Level.INFO, "Enter the Name: ");
+            holdername1 = sc.nextLine();
+            LOGGER.log(Level.INFO, "Enter the cardnumber:  ");
+            cardnumber1 = sc.nextLong();
+            sc.nextLine();
+            LOGGER.log(Level.INFO, "Enter the expiredate:");
+            expiredate1 = sc.nextLine();
+            CreditcardException cc2 = new CreditcardException((holdername1), cardnumber1, expiredate1);
+            LOGGER.log(Level.INFO, "current object creditcardnumber and new object creditcard numbers are same? :");
+            result2 = cc.equalis(cc2);
+            LOGGER.info(result2);
+        }
+        catch(InputMismatchException e)
+        {
+            throw new InputMismatchException("please Enter the valid input ");
+        }
+    }
 }
